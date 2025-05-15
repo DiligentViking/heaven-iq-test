3c50b395ea8205eedf03ddded9f8a526aaa1c445
DiligentViking <acore-n@proton.me>
2025-05-15 13:57:08 -0500

    Add index.js code to clear page when modal shown

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
+const container = document.querySelector('.container');
 const startTest = document.querySelector('.start-test');
 const note = document.querySelector('.note');
 
 startTest.addEventListener('click', (e) => {
   e.preventDefault();  /* so i guess the a is just for accessibility? (because <a> tags are for navigation, <button> tags are for other actions) */
+  const elems = Array.from(container.children);
+  elems.forEach((elem) => elem.setAttribute('style', 'visibility: hidden'));
   note.showModal();
 });
