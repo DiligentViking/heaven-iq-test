6f792ffb8a6d8d4bbaa4e1b56458d035700ad874
DiligentViking <acore-n@proton.me>
2025-05-16 13:30:27 -0500

    Initialize javascript vars

---
+++ scripts/quiz.js
@@ -0,0 +1,39 @@
+/* TEMPLATE */
+/*
+  {
+    beforePart: "",
+    quotePart: "",
+    afterPart: "",
+    choices: {
+      a: "",
+      b: "",
+      c: "",
+      d: "",
+      e: "",
+      f: "",
+    },
+    custom: {},
+    answer: "",
+  },
+*/
+
+const questions = [
+
+];
+
+
+////////////////////
+
+
+const container = document.querySelector('.container');
+/* Header */
+const header = document.querySelector('.header');
+const category = document.querySelector('.category');
+const progress = document.querySelector('.progress');
+/* Card */
+const qBeforePart = document.querySelector('.q-before-part');
+const qQuotePart = document.querySelector('.q-quote-part');
+const qAfterPart = document.querySelector('.q-after-part');
+const answerSelection = document.querySelector('.answer-selection');
+const nextBtn = document.querySelector('.next');
+const backBtn = document.querySelector('.back');
