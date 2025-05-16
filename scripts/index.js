cc17c9955491092df722888e1cb14efd4016528d
DiligentViking <acore-n@proton.me>
2025-05-16 11:12:17 -0500

    Initialize/link quiz files

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const containerDiv = document.querySelector('.container');
 const startTestLink = document.querySelector('.start-test');
 const noteModal = document.querySelector('.note');
+const okBtn = document.querySelector('.ok');
 
 startTestLink.addEventListener('click', (e) => {
   e.preventDefault();  /* so i guess the a is just for accessibility? (because <a> tags are for navigation, <button> tags are for other actions) */
@@ -9,4 +10,8 @@ startTestLink.addEventListener('click', (e) => {
   elems.forEach((elem) => elem.setAttribute('style', 'opacity: 0; visibility: hidden'));
   noteModal.showModal();
   noteModal.setAttribute('style', 'opacity: 1');  // i had to put it after
+});
+
+okBtn.addEventListener('click', () => {
+  window.location.href = './quiz.html';
 });
