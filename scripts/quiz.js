83e288542a6ccc5d2215a0802f33c60ff1894480
DiligentViking <acore-n@proton.me>
2025-05-16 18:23:48 -0500

    Make quiz.js displayContent func

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -17,8 +17,22 @@
   },
 */
 
-const questions = [
-
+const theQuestions = [
+  {
+    beforePart: "Complete the following quote from Jesus in the Gospel of John (NIV):",
+    quotePart: "For God so loved the world that He gave His one and only son, that whoever _____ shall not perish but have eternal life. For God did not send His Son into the world to condemn the world, but to save the world through Him.",
+    afterPart: "",
+    choices: {
+      a: "believes in Him",
+      b: "is a Christian",
+      c: "keeps the Commandments",
+      d: "is a good person",
+      e: "accepts Him as their Lord and Savior",
+      f: "does not contribute to Climate Change",
+    },
+    custom: {},
+    answer: "a",
+  },
 ];
 
 
@@ -37,3 +51,30 @@ const qAfterPart = document.querySelector('.q-after-part');
 const answerSelection = document.querySelector('.answer-selection');
 const nextBtn = document.querySelector('.next');
 const backBtn = document.querySelector('.back');
+
+
+function displayContent(question) {
+  qBeforePart.textContent = question.beforePart;
+  qAfterPart.textContent = question.afterPart;
+  qQuotePart.textContent = question.quotePart;
+
+  const choices = Object.entries(question.choices);  // so i can use for loop structure without having a hasOwnProperty thing
+  const isEmpty = (choices.length === 0);
+  if (!isEmpty) {
+    for (const [key, value] of choices) {
+      const radio = document.createElement('input');
+      radio.setAttribute('id', key);
+      radio.setAttribute('type', 'radio');
+      radio.setAttribute('name', 'radio');
+      ////
+      const label = document.createElement('label');
+      label.setAttribute('for', key);
+      label.textContent = `${key}) ${value}`;
+      ////
+      answerSelection.append(radio, label);
+    }
+  }
+}
+
+
+displayContent(theQuestions[0]);
