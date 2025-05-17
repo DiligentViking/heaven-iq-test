90f9f6521799bba429679e675ec2cc6def545b5f
DiligentViking <acore-n@proton.me>
2025-05-17 16:37:51 -0500

    Add crucial next/back functionality!

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,11 +1,12 @@
 /* TEMPLATE */
 /*
   {
+    category: "JESUS",
     beforePart: "",
     quotePart: ["",],  // you may need quotes in places
     afterPart: "",
     choices: {
-      a: "",
+      a: "",  // no cap first letter, only prop nouns
       b: "",
       c: "",
       d: "",
@@ -34,10 +35,29 @@ const theQuestions = [
     custom: {},
     answer: "a",
   },
+  {
+    category: 'JESUS',
+    beforePart: "Complete the following statement from Jesus in the Gospel of Matthew (NIV):",
+    quotePart: ["\"Not everyone who says to me, 'Lord, Lord,' will enter the Kingdom of Heaven, but only _____\"",],
+    afterPart: "",
+    choices: {
+      a: "those who accept me as their Lord and Savior",
+      b: "those who are tolerant of different races, sexes, and religions",
+      c: "those who are not evil",
+      d: "the one who does the will of my Father who is in Heaven",
+      e: "Jesus did not say this and everybody will go to Heaven because there is no such thing as Hell.",
+    },
+    custom: {},
+    answer: "d",
+  },
 ];
 
 
 ////////////////////
+const userAnswers = [];
+
+let questionNumber = 0;
+let userSelection;
 
 
 const container = document.querySelector('.container');
@@ -49,7 +69,9 @@ const progress = document.querySelector('.progress');
 const qBeforePart = document.querySelector('.q-before-part');
 const qQuotePart = document.querySelector('.q-quote-part');
 const qAfterPart = document.querySelector('.q-after-part');
+////
 const answerSelection = document.querySelector('.answer-selection');
+////
 const nextBtn = document.querySelector('.next');
 const backBtn = document.querySelector('.back');
 
@@ -60,6 +82,7 @@ function displayContent(question) {
 
   qBeforePart.textContent = question.beforePart;
   ////
+  qQuotePart.textContent = '';
   question.quotePart.forEach((para) => {
     const p = document.createElement('p');
     p.textContent = para;
@@ -69,6 +92,7 @@ function displayContent(question) {
   if (!question.afterPart) qAfterPart.setAttribute('style', 'display: none');
   else qAfterPart.textContent = question.afterPart;
 
+  answerSelection.textContent = '';
   const choices = Object.entries(question.choices);  // so i can use for loop structure without having a hasOwnProperty thing
   const isEmpty = (choices.length === 0);
   if (!isEmpty) {
@@ -89,7 +113,51 @@ function displayContent(question) {
       answerSelection.append(choiceDiv);
     }
   }
+
+  if (questionNumber === 0) {
+    backBtn.setAttribute('style', 'display: none');
+  } else {
+    backBtn.setAttribute('style', 'display: normal');
+  }
+
+  if (userSelection) {
+    const prevSelectedRadio = document.getElementById(userSelection);
+    prevSelectedRadio.checked = true;
+  }
 }
 
 
-displayContent(theQuestions[0]);
+function setUserAnswer() {  // now this part may be a bit clunky
+  if (userSelection) {
+    userAnswers[questionNumber] = userSelection;
+    userSelection = undefined;
+    console.log(userAnswers);
+    return true;
+  }
+}
+
+
+function QuizController() {
+  displayContent(theQuestions[questionNumber]);
+
+  answerSelection.addEventListener('click', (e) => {
+    userSelection = (e.target.id) ? e.target.id : undefined;  // 'user selected label or nothing'
+  });
+
+  nextBtn.addEventListener('click', () => {
+    const isAnswered = setUserAnswer();
+    if (!isAnswered) alert('Question skipped!');
+    questionNumber++;
+    userSelection = userAnswers[questionNumber];
+    displayContent(theQuestions[questionNumber]);
+  });
+
+  backBtn.addEventListener('click', () => {
+    questionNumber--;
+    userSelection = userAnswers[questionNumber];
+    displayContent(theQuestions[questionNumber]);
+  });
+}
+
+
+QuizController();
