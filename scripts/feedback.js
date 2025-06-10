75825c110b9643543dfa95d98e8dc0b4db311e30
DiligentViking <acore-n@proton.me>
2025-06-10 12:06:57 -0500

    Optimize big background png; Initialize answers pg

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -3,6 +3,7 @@ const btnSend = document.querySelector('.send');
 
 const prev_page = localStorage.getItem('prev-page');
 const referrer = prev_page ? prev_page : 'about';
+localStorage.clear();
 
 btnCancel.addEventListener('click', () => {
   window.location.href = `./${referrer}.html`;
