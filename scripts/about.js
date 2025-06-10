d973088f4240347ff76e9ce5ac2e98c47b6c5ce1
DiligentViking <acore-n@proton.me>
2025-06-10 09:23:02 -0500

    Rename/refactored 'about-us' to 'about'

---
+++ scripts/about.js
@@ -0,0 +1,2 @@
+document.querySelector('.feedback').addEventListener('click', () => window.location.href = './feedback.html');
+document.querySelector('.back').addEventListener('click', () => window.location.href = './index.html');
--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,10 +1,10 @@
 const btnCancel = document.querySelector('.cancel');
 btnCancel.addEventListener('click', () => {
-  window.location.href = './about-us.html';
+  window.location.href = './about.html';
 });
 
 const btnSend = document.querySelector('.send');
 btnSend.addEventListener('click', () => {
   alert('Message sent!');  // i need to learn about some backend
-  window.location.href = './about-us.html';
+  window.location.href = './about.html';
 });
